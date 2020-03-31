f9ad8c02bec68bcfc2933a13472b833569d73ca3
yanai101 <yanai101@gmail.com>
2020-03-31 21:10:05 +0300

    update marketplace

--- src/components/common/activity/activity.js
+++ src/components/common/activity/activity.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import cls from './activity.module.scss';
 import { FaPlus } from 'react-icons/fa';
+import { CATEGORIES } from '../../../utils/enums';
 
-export default function Activity({time = 0 , imageSrc= 'https://loremflickr.com/320/240' , description="description placeholder", colorActivity="pink"}) {
+export default function Activity({plusClick = null, activity}) {
 
     const [offset , setOffset] = React.useState(1000);
+    const imageSrc= activity.imageSrc || 'https://loremflickr.com/320/240' , 
+        description= activity.description || "description placeholder", 
+        colorActivity= CATEGORIES[activity.category.toLowerCase()].color || "pink",
+        time = activity.time;
 
     React.useEffect(() => {
         setTimeout(()=>{
             const newOffset = time >60 ? 0 : 1000 - (time * 1255 / 100);
             setOffset(newOffset);
         })
-    }, [time])
+    }, [])
 
     const openMore = () => {console.log('more clicked')}
 
-    const addActivityToList = () => {
-        console.log('addActivityToList')
+    const addActivityToList = (e) => {
+        const pos = {
+            x: e.clientX,
+            y : e.clientY
+        }
+        plusClick && plusClick(pos);
     }
-    
 
     return (
         <div className={cls.activityBox}>
--- src/components/pages/marketplace/marketplace.js
+++ src/components/pages/marketplace/marketplace.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import style from './marketplace.module.scss';
 import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import { FaSearch, FaPlus } from 'react-icons/fa';
 import Activity from '../../common/activity/activity';
+import { Addtolist } from '../../common/addtolist/addtolist';
+
+
+const activities = [
+    {
+        id: 1,
+        time :30 ,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        category: 'sport'   
+    },
+    {
+        id: 2,
+        time :60 ,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        category: 'cooking'   
+    },
+    {
+        id: 3,
+        time :45 ,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        category: 'diy'   
+    },
+    {
+        id: 5,
+        time :99 ,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        category: 'education'   
+    }
+
+]
 
 const Marketplace  = () => {
 
+    const [listPopupPos , setListPopupPos] = useState(false);
+
     const searchChange =(e) =>{
         console.log(e.target.value);
-    }   
+    }
+    
+    const toggleActivityToList = (pos = null) =>{
+        setListPopupPos(pos);
+    }
 
     return (
         <>
@@ -29,16 +65,11 @@ const Marketplace  = () => {
             <small>Pick your favorite activities and add them to your lists</small>
         </h2>
         <div className={style.activities}>
-            <Activity time={30} description="Activity is veary cool to be ther tand a asa asdasd a asdasd asdasd asdasd asdkpoi  sdasd "/>
-            <Activity time={90}/>
-            <Activity time={10}/>
-            <Activity time={30}/>
-            <Activity time={90}/>
-            <Activity time={10}/>
-            <Activity time={30}/>
-            <Activity time={90}/>
-            <Activity time={10}/>
+            {
+                activities.map(activity => <Activity key={activity.id} activity={activity} plusClick={toggleActivityToList}/>)
+            }
         </div>
+        <Addtolist pos={listPopupPos}/>
         </>
     )
 }
