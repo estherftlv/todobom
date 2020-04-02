73481ace108cc9df6b43760bdc5c18d60e88b526
yanai101 <yanai101@gmail.com>
2020-04-02 15:19:57 +0300

    list update

--- src/components/pages/lists/listItem.js
+++ src/components/pages/lists/listItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './listItem.scss'
 import { ReactComponent as Present } from '../../common/sideNav/present.svg'
 
@@ -7,19 +7,24 @@ export const ListItem = () => {
     const rewards = [45 , 120 , 245]
     const max = Math.max(...rewards)
     const min = Math.min(...rewards)
+    const [progress, setProgress] = useState(120)
+    const one = max / 100;
 
     const rewardsPosition = rewards.map(reward =>{
-        const one = max / 100;
         return {pos : reward / one, value: reward }
     })
 
     return (
         <div className="listItem">
         <header>
-            <h1>amalia</h1>  
+            <h2>amalia</h2>  
             <div class="prograss">
-                {rewardsPosition.map(item=><span key={item.pos} style={{left:`${item.pos}%`}}><Present/></span>)}
-                <div class="fill"/>
+            {rewardsPosition.map(item=>
+                <span key={item.pos} className={progress >= item.value ? 'green' : ''} style={{left:`${item.pos}%`}}>
+                    <Present/>
+                </span>)
+            }
+                <div class="fill" style={{width:`${progress / one }%`}}/>
             </div>  
         </header>
         
