8dc9c1967c6e11e8bc39e981d6aada0f63061e08
estherf tlv <50096043+estherftlv@users.noreply.github.com>
2020-04-04 20:27:28 +0300

    sum total time of activities in a list

--- src/components/pages/lists/listItem.js
+++ src/components/pages/lists/listItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import './listItem.scss'
 import { ReactComponent as Present } from '../../common/sideNav/present.svg'
 import { ActivityInList } from './activityInList'
@@ -8,7 +8,7 @@ export const ListItem = ({data, openMenuFunc}) => {
 
     const lists = [45 , 120 , 245];
     const max = Math.max(...lists);
-    const min = Math.min(...lists);
+    //const min = Math.min(...lists);
     const [progress, setProgress] = useState(120);
     const one = max / 100;
 
@@ -16,12 +16,22 @@ export const ListItem = ({data, openMenuFunc}) => {
 
 
     // List property
-    const [activities, setActivities] = useState([]);
 
     const listPosition = lists.map(lists =>({pos : lists / one, value: lists }));
 
 
+    const updateSum = useCallback(() => {
+      let initialValue = 0;
+      let sum = 0;
+      if(data.assignedActs){
 
+          sum = data.assignedActs.reduce((accumulator, currentValue)=> {
+          return accumulator + currentValue.time;
+        }, initialValue);
+      }
+      return sum;
+
+  	}, [data]);
 
     return (
         <div className="listItem">
@@ -46,7 +56,7 @@ export const ListItem = ({data, openMenuFunc}) => {
                     <div className="checkBoxArea">
                         <input type="checkbox" className="checkBox"/>
                         <img src={require("./images/time-gray.png")} alt="clock"/>
-                        <p>60 min</p>
+                        <p>{updateSum()}</p>
                     </div>
 
                     <div className="titleArea">
@@ -56,7 +66,7 @@ export const ListItem = ({data, openMenuFunc}) => {
                     </div>
                 </div>
                 {
-                  (data.assignedActs) &&  data.assignedActs.map(act=><ActivityInList data={act}/>)
+                  (data.assignedActs) &&  data.assignedActs.map(act=><ActivityInList data={act} key={act.id}/>)
                 }
 
             </header>
